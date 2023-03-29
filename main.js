977256d0a1dfeba35ba0d90c5acc7cba3bd648b5
Mateo Gomez <mateogomezmolina@gmail.com>
2023-03-28 23:17:29 -0500

    Add window and basic config

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('hola desde electron')
+const path = require('node:path')
+const { app, BrowserWindow } = require('electron')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+})
